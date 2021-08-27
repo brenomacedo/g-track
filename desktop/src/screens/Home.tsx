c4b4883fe56fedbcb396b39a45f602b340e42f8b
Breno <b88660501@gmail.com>
2021-08-27 09:23:33 -0300

    Search bar

--- desktop/src/screens/Home.tsx
+++ desktop/src/screens/Home.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import Bar from '../components/Bar'
 import SideBar from '../components/Sidebar'
 import styled from 'styled-components'
+import { FiSearch, FiArrowLeft, FiArrowRight } from 'react-icons/fi'
 
 const Container = styled.div`
     min-height: 100vh;
@@ -13,6 +14,58 @@ const Container = styled.div`
         display: flex;
         flex: 1;
     }
+
+    .home-content {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .top-bar {
+        display: flex;
+        padding: 0.5rem;
+        align-items: center;
+        gap: 2rem;
+    }
+
+    .search {
+        position: relative;
+    }
+
+    .search-icon {
+        color: black;
+        font-size: 1rem;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
+    .search-input {
+        width: 20rem;
+        border-radius: 150px;
+        font-family: 'OpenSans';
+        font-weight: bold;
+        border: 0;
+        outline: none;
+        padding: 10px;
+        padding-left: 45px;
+    }
+
+    .back-forward {
+        display: flex;
+        gap: 1rem;
+    }
+
+    .arrow {
+        font-size: 2rem;
+        color: #ccc;
+        cursor: pointer;
+    }
+
+    .arrow:hover {
+        color: white;
+    }
 `
 
 const Home: FC = () => {
@@ -21,6 +74,18 @@ const Home: FC = () => {
             <Bar />
             <div className="home">
                 <SideBar selected='home' />
+                <div className="home-content">
+                    <div className="top-bar">
+                        <div className="back-forward">
+                            <FiArrowLeft className='arrow' />
+                            <FiArrowRight className='arrow' />
+                        </div>
+                        <div className="search">
+                            <input type="text" className="search-input" placeholder='Search a music' />
+                            <FiSearch className='search-icon' />
+                        </div>
+                    </div>
+                </div>
             </div>
         </Container>
     )
