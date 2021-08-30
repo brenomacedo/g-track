d82dc2cb90b18808bc266b9d7d2270abd6580c2d
Breno <b88660501@gmail.com>
2021-08-30 12:58:12 -0300

    Adding musics

--- desktop/src/index.ts
+++ desktop/src/index.ts
@@ -57,7 +57,7 @@ const createWindow = (): void => {
       mainWindow.show()
   })
 
-  mainWindow.webContents.openDevTools()
+//   mainWindow.webContents.openDevTools()
 
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
